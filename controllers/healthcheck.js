73a394bef7d3ee50190291e3a6d532000c67fee7
Amy <amy.su0123@gmail.com>
2019-04-19 14:32:31 +0800

    封装路由文件（controllers），并测试/healthcheck接口访问正常

--- controllers/healthcheck.js
+++ controllers/healthcheck.js
@@ -2,11 +2,17 @@
  * @Description: 健康接口检查
  * @Author: amy
  * @Date: 2019-04-18 10:54:38
- * @LastEditTime: 2019-04-18 10:56:12
+ * @LastEditTime: 2019-04-19 14:31:19
  */
 
+ const logger = require('../utils/logger')();
+
  module.exports = (router) => {
+    //  测试
+    //  curl http://localhost:3000/healthcheck
      router.get('/', async (ctx, next) => {
-         ctx.body = 'pass';
-     })
+         console.log(111111)
+         ctx.body = 'hello world';
+         logger.info('*************** 接口正常 ******************')
+     });
  }
--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const logger = require('./utils/logger')();
 logger.info('********* index.js start *********');
 
 global.config = require('./config')(process.env.NODE_ENV);
-logger.info('config:', global.config);
+logger.info('config-evn:', process.env.NODE_ENV);
 
 const Koa = require('koa');
 // 创建实例
 const app = new Koa();
 
-
-// 引入controllers路由集合
-// const Router = require('koa-router');
-
+// 封装routers
+const fs = require('fs');
+const path = require('path');
+const Router = require('koa-router');
+((root = './controllers') => {
+    // 读取[root]目录下所有js文件
+    fs.readdirSync(root).forEach((filename) => {
+        let file = path.parse(filename);
+        logger.info('load router (controllers): ' + filename);
+        if(file.ext.toLowerCase() !== '.js') return;
+        const router = new Router({prefix: `/${file.name}`});
+        require(`${root}/${file.name}`)(router);
+        app.use(router.routes());
+    })
+})()
 
 app.listen(global.config.port);
-logger.info('启动服务，端口号为：', global.config.port);
+logger.info('********* 启动服务，端口号为：', global.config.port + ' *********');
 
